7278646cabe4d2c7db1705b811f09214998cf84d
vrortega <enigmadanceteam@gmail.com>
2024-06-10 17:29:44 -0300

    feat: layout and navigation

---
+++ Desafio2PDI/Controllers/FlightViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FlightViewController.swift
+//  Desafio2PDI
+//
+//  Created by Vitoria Ortega on 10/06/24.
+//
+
+import UIKit
+
+class FlightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Desafio2PDI/Models/Passenger.swift
@@ -0,0 +1,13 @@
+//
+//  Passenger.swift
+//  Desafio2PDI
+//
+//  Created by Vitoria Ortega on 10/06/24.
+//
+
+import Foundation
+
+struct Passenger {
+    let name: String
+    let age: Int
+}
