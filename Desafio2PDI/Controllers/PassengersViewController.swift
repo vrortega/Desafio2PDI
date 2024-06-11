edd43b07de7aaf32836a927a79a2bf896e0fa3c6
vrortega <enigmadanceteam@gmail.com>
2024-06-11 13:31:34 -0300

    feat: add passengers in tableview

--- Desafio2PDI/Controllers/PassengersViewController.swift
+++ Desafio2PDI/Controllers/PassengersViewController.swift
@@ -11,9 +11,16 @@ class PassengersViewController: UIViewController {
 
     @IBOutlet weak var nameTf: UITextField!
     @IBOutlet weak var ageTf: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var noPassangersLb: UILabel!
+    
+    var passengers: [Passenger] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        updateView()
     }
     
     @IBAction func board(_ sender: UIButton) {
@@ -33,7 +40,9 @@ class PassengersViewController: UIViewController {
         let passenger = Passenger(name: name, age: age)
         if passenger.isAdult() {
             print("\(passenger.name) pode embarcar")
-            showAlert(message: "\(passenger.name) pode embarcar")
+            passengers.append(passenger)
+            tableView.reloadData()
+            updateView()
         } else {
             showAlert(message: "\(passenger.name) nao pode embarcar porque é menor de idade")
         }
@@ -43,5 +52,36 @@ class PassengersViewController: UIViewController {
         let alert = UIAlertController(title: "Alerta", message: message, preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
+    }
+    
+    
+    func updateView(){
+        if passengers.isEmpty {
+            tableView.isHidden = true
+            noPassangersLb.isHidden = false
+        } else {
+            tableView.isHidden = false
+            noPassangersLb.isHidden = true
+        }
+    }
+}
+
+extension PassengersViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return passengers.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let passenger = passengers[indexPath.row]
+        cell.textLabel?.text = passenger.name
+        cell.detailTextLabel?.text = "Idade \(passenger.age)"
+        return cell
+    }
+    
+    // Delegate
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let passenger = passengers[indexPath.row]
+        print("\(passenger.name) selecionado")
     }
 }
